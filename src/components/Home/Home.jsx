1b44744452f596ac683d0fbb281be284e574202e
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-23 00:33:13 +0600

    Carousel Added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Banner from './Banner';
+import { Box, styled } from '@mui/material';
 
+
+
+const Container = styled(Box)`
+    padding: 10px;
+    background: #f2f2f2;
+`
 const Home = () => {
     return (
-        <div>
+        <>
             <NavBar/>
-        </div>
+            <Container>
+                <Banner/>
+            </Container>
+        </>
     );
 };
 
