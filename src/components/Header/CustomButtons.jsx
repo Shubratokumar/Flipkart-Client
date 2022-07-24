4d3443f61c0bf518737a72a37c919de061159e7b
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-24 15:39:52 +0600

    getting input filed data

--- src/components/Header/CustomButtons.jsx
+++ src/components/Header/CustomButtons.jsx
@@ -1,57 +1,62 @@
-import React, { useState } from 'react';
-import { Box, Button, Typography, styled } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LoginDialog from './../Login/LoginDialog';
+import React, { useState } from "react";
+import { Box, Button, Typography, styled } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LoginDialog from "./../Login/LoginDialog";
 
 const Wrapper = styled(Box)`
-    display: flex;
-    margin: 0 3% 0 auto;    
-    & > button, & > p, & > div {
-        margin-right: 40px;
-        font-size: 15px;
-        font-weight: 600;
-        align-items: center;
-    }
-`
+  display: flex;
+  margin: 0 3% 0 auto;
+  & > button,
+  & > p,
+  & > div {
+    margin-right: 40px;
+    font-size: 15px;
+    font-weight: 600;
+    align-items: center;
+  }
+`;
 
 const Container = styled(Box)`
-    display: flex;
-`
+  display: flex;
+`;
 
 const LoginButton = styled(Button)`
-    color: #2874f0;
-    background: #fff;
-    text-transform: none;
-    padding: 5px 40px;
-    border-radius: 2px;
-    box-shadow: none;
-    font-weight: 600;
-    height: 32px;
+  color: #2874f0;
+  background: #fff;
+  text-transform: none;
+  padding: 5px 40px;
+  border-radius: 2px;
+  box-shadow: none;
+  font-weight: 600;
+  height: 32px;
 `;
 
 const Text = styled(Typography)`
-    font-weight: 600;
-    padding-left: 3px;
+  font-weight: 600;
+  padding-left: 3px;
 `;
 
-
 const CustomButtons = () => {
-    const [ open, setOpen ] = useState(false);
-    const openDialog = () =>{
-        setOpen(true);
-    }
-    return (
-        <Wrapper>
-           <LoginButton variant='contained' onClick={()=> openDialog()}>Login</LoginButton>
-           <Typography style={{marginTop: 3, width: 135}}>Become a Seller</Typography>
-           <Typography style={{marginTop: 3}}>More</Typography>
-           <Container>
-                <ShoppingCartIcon />
-                <Text >Cart</Text>
-           </Container>
-           <LoginDialog open={open} setOpen={setOpen} />
-        </Wrapper>
-    );
+  const [open, setOpen] = useState(false);
+  const openDialog = () => {
+    setOpen(true);
+  };
+  return (
+    <Wrapper>
+      <LoginButton variant="contained" onClick={() => openDialog()}>
+        Login
+      </LoginButton>
+      <Typography style={{ marginTop: 3, width: 135 }}>
+        Become a Seller
+      </Typography>
+      <Typography style={{ marginTop: 3 }}>More</Typography>
+      <Container>
+        <ShoppingCartIcon />
+        <Text>Cart</Text>
+      </Container>
+      <LoginDialog open={open} setOpen={setOpen} />
+    </Wrapper>
+  );
 };
 
 export default CustomButtons;
--- src/components/Login/LoginDialog.jsx
+++ src/components/Login/LoginDialog.jsx
@@ -1,124 +1,203 @@
-import React, { useState } from 'react';
-import { Dialog, Box, TextField, Button, Typography, styled } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Dialog,
+  Box,
+  TextField,
+  Button,
+  Typography,
+  styled,
+} from "@mui/material";
 
 const Component = styled(Box)`
-    height: 70vh;
-    width: 90vh;
+  height: 70vh;
+  width: 90vh;
 `;
 
 const Image = styled(Box)`
-    background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85%  no-repeat;
-    height: 83%;
-    width: 28%;
-    padding: 45px 35px;
-    & > p, & > h5 {
-        color: white;
-        font-weight: 600;
-    }
+  background: #2874f0
+    url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png)
+    center 85% no-repeat;
+  height: 83%;
+  width: 28%;
+  padding: 45px 35px;
+  & > p,
+  & > h5 {
+    color: white;
+    font-weight: 600;
+  }
 `;
 
 const Wrapper = styled(Box)`
-    display: flex;
-    flex-direction: column;
-    padding: 20px 30px;
-    flex: 1;
-    & > div, & > button, & > p {
-        margin-top: 20px;
-    }
+  display: flex;
+  flex-direction: column;
+  padding: 20px 30px;
+  flex: 1;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
 `;
 
 const LoginButton = styled(Button)`
-    text-transform: none;
-    background: #FB641B;
-    color: #fff;
-    height: 48px;
-    border-radius: 2px;
-`
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
+`;
 const RequestOTP = styled(Button)`
-    text-transform: none;
-    background: #fff;
-    color: #2874f0 ;
-    height: 48px;
-    border-radius: 2px;
-    box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
 `;
 
 const Text = styled(Box)`
-    font-size: 12px;
-    color: #878787;
+  font-size: 12px;
+  color: #878787;
 `;
 const CreateAccount = styled(Typography)`
-    font-size: 14px;
-    text-align: center;
-    color: #2874f0;
-    font-weight: 600;
-    cursor: pointer;
+  font-size: 14px;
+  text-align: center;
+  color: #2874f0;
+  font-weight: 600;
+  cursor: pointer;
 `;
 
 const accountInitialValues = {
-    login: {
-        view: "login",
-        heading: "Login",
-        subHeading: "Get access to your Orders, Wishlist and Recommendations"
+  login: {
+    view: "login",
+    heading: "Login",
+    subHeading: "Get access to your Orders, Wishlist and Recommendations",
+  },
+  signup: {
+    view: "signup",
+    heading: "Looks like you're new here!",
+    subHeading: "Sign up with your mobile number to get started",
+  },
+};
 
-    },
-    signup: {
-        view: "signup",
-        heading: "Looks like you're new here!",
-        subHeading: "Sign up with your mobile number to get started"
-    }
-}
+const signupInitialValue = {
+  firstname: "",
+  lastname: "",
+  username: "",
+  email: "",
+  password: "",
+  phone: "",
+};
 
+const LoginDialog = ({ open, setOpen }) => {
+  const [account, toggleAccount] = useState(accountInitialValues.login);
+  const [signup, setSignup] = useState(signupInitialValue);
 
-const LoginDialog = ({open, setOpen}) => {
-    const[ account, toggleAccount ] = useState(accountInitialValues.login); 
-    const toggleSignup = () =>{
-        toggleAccount(accountInitialValues.signup);
-    };
-    const toggleLogin = () =>{
+  const toggleSignup = () => {
+    toggleAccount(accountInitialValues.signup);
+  };
+  /* const toggleLogin = () =>{
         toggleAccount(accountInitialValues.login);
-    };
+    }; */
 
-    const handleClose = () =>{
-        setOpen(false);
-        toggleAccount(accountInitialValues.login);
-    };
-    return (
-        <Dialog open={open} onClose={handleClose} PaperProps={{sx:{ maxWidth: 'unset'}}}>
-            <Component>
-                <Box style={{ display: 'flex', height: "100%" }}>
-                    <Image>
-                        <Typography variant="h5">{account.heading}</Typography>
-                        <Typography style={{ marginTop: 20 }}>{account.subHeading}</Typography>
-                    </Image>
-                    { account.view === 'login' ? 
-                        <Wrapper>
-                            <TextField label="Enter Email/Mobile Number" variant="standard"/>
-                            <TextField label="Enter Password" variant="standard"/>
-                            <Text>By continuing, you agree to Flipkart's Terms of Use and Privicy Policy.</Text>
-                            <LoginButton>Login</LoginButton>
-                            <Typography style={{ textAlign: 'center' }}>OR</Typography>
-                            <RequestOTP>Request OTP</RequestOTP>
-                            <CreateAccount onClick={()=> toggleSignup()} >New to Flipkart? Create an account</CreateAccount>
-                        </Wrapper> 
-                    :
-                        <Wrapper>
-                            <TextField label="Enter Firstname" variant="standard"/>
-                            <TextField label="Enter Lastname" variant="standard"/>
-                            <TextField label="Enter Username" variant="standard"/>
-                            <TextField label="Enter Email" variant="standard"/>
-                            <TextField label="Enter Password" variant="standard"/>
-                            <TextField label="Enter Phone" variant="standard"/>
-                            <Text>By continuing, you agree to Flipkart's Terms of Use and Privicy Policy.</Text>
-                            <LoginButton>Continue</LoginButton>
-                            <Typography style={{ textAlign: 'center' }}>OR</Typography>
-                            <RequestOTP onClick={()=> toggleLogin()} >Existing User? Login</RequestOTP>
-                        </Wrapper>
-                    }
-                </Box>
-            </Component>
-        </Dialog>
-    );
+  const handleClose = () => {
+    setOpen(false);
+    toggleAccount(accountInitialValues.login);
+  };
+
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+    console.log(signup);
+  };
+  
+  const signupUser = () =>{
+
+  }
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      PaperProps={{ sx: { maxWidth: "unset" } }}
+    >
+      <Component>
+        <Box style={{ display: "flex", height: "100%" }}>
+          <Image>
+            <Typography variant="h5">{account.heading}</Typography>
+            <Typography style={{ marginTop: 20 }}>
+              {account.subHeading}
+            </Typography>
+          </Image>
+          {account.view === "login" ? (
+            <Wrapper>
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                label="Enter Email/Mobile Number"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                label="Enter Password"
+                variant="standard"
+              />
+              <Text>
+                By continuing, you agree to Flipkart's Terms of Use and Privicy
+                Policy.
+              </Text>
+              <LoginButton>Login</LoginButton>
+              <Typography style={{ textAlign: "center" }}>OR</Typography>
+              <RequestOTP>Request OTP</RequestOTP>
+              <CreateAccount onClick={() => toggleSignup()}>
+                New to Flipkart? Create an account
+              </CreateAccount>
+            </Wrapper>
+          ) : (
+            <Wrapper>
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="firstname"
+                label="Enter Firstname"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="lastname"
+                label="Enter Lastname"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="username"
+                label="Enter Username"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="email"
+                label="Enter Email"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="password"
+                label="Enter Password"
+                variant="standard"
+              />
+              <TextField
+                onChange={(e) => onInputChange(e)}
+                name="phone"
+                label="Enter Phone"
+                variant="standard"
+              />
+              {/* <Text>By continuing, you agree to Flipkart's Terms of Use and Privicy Policy.</Text> */}
+              <LoginButton onClick={()=> signupUser()}>Continue</LoginButton>
+              {/* <Typography style={{ textAlign: 'center' }}>OR</Typography>
+                            <RequestOTP onClick={()=> toggleLogin()} >Existing User? Login</RequestOTP> */}
+            </Wrapper>
+          )}
+        </Box>
+      </Component>
+    </Dialog>
+  );
 };
 
 export default LoginDialog;
