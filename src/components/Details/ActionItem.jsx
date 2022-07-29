f9628f44bbd42c24f3cd1aab128a37266536260c
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-30 03:11:05 +0600

    Detail view component left side done

--- src/components/Details/ActionItem.jsx
+++ src/components/Details/ActionItem.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Box, Button, styled } from '@mui/material';
+import { ShoppingCart as Cart, FlashOn as Flash } from '@mui/icons-material';
 
-const ActionItem = () => {
+const LeftContainer = styled(Box)`
+    min-width: 40%;
+    padding: 40px 0 0 80px;
+`;
+const Image = styled("img")({  
+     width: '95%',  
+    padding: '8px',    
+});
+const StyledButton = styled(Button)`
+    width: 48%;
+    height: 50px;
+    border-radius: 2px;
+`;
+
+
+const ActionItem = ({ product }) => {
     return (
-        <div>
-            
-        </div>
+        <LeftContainer>
+            <Box  style={{ padding: '15px 20px', border: '1px solid #f0f0f0' }}>
+                <Image src={product.detailUrl} alt="product-detail" />
+            </Box>
+            <StyledButton variant="contained" style={{ marginRight: 12, background: '#ff9f00' }}>
+               <Cart/> Add to Cart
+            </StyledButton>
+            <StyledButton variant="contained" style={{ background: '#fb541b'}} >
+               <Flash/> Buy Now
+            </StyledButton>
+        </LeftContainer>
     );
 };
 
--- src/components/Details/DetailView.jsx
+++ src/components/Details/DetailView.jsx
@@ -2,7 +2,22 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getProductDetails } from '../../redux/actions/productActions';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, styled, Grid } from '@mui/material';
+import ActionItem from './ActionItem';
+
+const Component = styled(Box)`
+    background: #f2f2f2;
+    margin-top: 55px;
+`;
+const Container = styled(Grid)`
+    background: #ffffff;
+    display: flex;
+`;
+const RightComponent = styled(Grid)`
+    margin-top: 50px;
+`;
+
+const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
 
 const DetailView = () => {
 
@@ -15,19 +30,33 @@ const DetailView = () => {
         if(product && id !== product.id){
             dispatch(getProductDetails(id));
             }
-    },[dispatch, id, product, loading ])
+    }, [dispatch, id, product, loading ]);
+
     return (
-        <Box>
+        <Component>
             {
                 product && Object.keys(product).length && 
-                    <Box>
-                        <Box></Box>
-                        <Box>
+                    <Container container>
+                        <Grid item lg={4} md={4} sm={8} xs={12}>
+                            <ActionItem product={product} key={product.id} />
+                        </Grid>
+                        <RightComponent item lg={8} md={8} sm={8} xs={12}>
                             <Typography>{product.title.longTitle}</Typography>
-                        </Box>
-                    </Box>
+                            <Typography style={{marginTop:5, color: '#878787', fontSize: 14}}>
+                                8 Ratings & 3 Reviews
+                                <Box component='span'>
+                                    <img src={fassured} alt="fassured" style={{width: 77, marginLeft: 20}} />
+                                </Box>
+                            </Typography>
+                            <Typography>
+                                <Box component='span' style={{fontSize: 28}}>${product.price.cost}</Box>&nbsp;&nbsp;&nbsp;
+                                <Box component='span' style={{color: '#878787'}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                                <Box component='span' style={{color: '#388E3C'}}>{product.price.discount}</Box>
+                            </Typography>
+                        </RightComponent>
+                    </Container>
             }
-        </Box>
+        </Component>
     );
 };
 
