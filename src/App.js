16c244a81a46195cc79cd46ef9e8e62d9c3f7392
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-29 09:56:46 +0600

    Routing for home and detailview done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Header from "./components/Header/Header";
 import Home from './components/Home/Home';
 import { Box } from '@mui/material';
 import DataProvider from './context/DataProvider';
+import { Routes, Route } from "react-router-dom";
+import DetailView from './components/Details/DetailView';
 
 function App() {
   return (
     <DataProvider>
-        <Header/>
-        <Box style={{marginTop: 55}}>
-          <Home/>
-        </Box>
+          <Header/>
+          <Box style={{marginTop: 55}}>
+            <Routes>
+              <Route path='/' element={<Home/>} />
+              <Route path='/product/:id' element={<DetailView/>} />
+            </Routes>
+          </Box>
     </DataProvider>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, styled, Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 // components
 import Search from './Search';
@@ -10,9 +11,11 @@ const StyledHeader = styled(AppBar)`
     background: #2874f0;
     height: 55px;
 `
-const Component = styled(Box)`
+const Component = styled(Link)`
     margin-left: 12%;
     line-height: 0;
+    text-decoration: none;
+    color: inherit;
 `
 
 const SubHeading = styled(Typography)`
@@ -38,7 +41,7 @@ const Header = () => {
     return (
         <StyledHeader>
             <Toolbar style={{ minHeight: 55 }}>
-                <Component>
+                <Component to='/'>
                     <img src={logoURL} alt="logo" style={{ width: 75 }} />
                     <Box style={{ display: 'flex' }}>
                         <SubHeading> Explore&nbsp;
