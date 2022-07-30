5badce2969938dcbd91096bcacda9ec0c211eb05
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-30 18:40:21 +0600

    product detail done

--- src/components/Details/DetailView.jsx
+++ src/components/Details/DetailView.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getProductDetails } from '../../redux/actions/productActions';
-import { Box, Typography, styled, Grid } from '@mui/material';
+import { Box, styled, Grid } from '@mui/material';
 import ActionItem from './ActionItem';
+import ProductDetail from './ProductDetail';
 
 const Component = styled(Box)`
     background: #f2f2f2;
@@ -17,8 +18,6 @@ const RightComponent = styled(Grid)`
     margin-top: 50px;
 `;
 
-const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
-
 const DetailView = () => {
 
     const dispatch = useDispatch();
@@ -29,7 +28,7 @@ const DetailView = () => {
     useEffect(()=>{
         if(product && id !== product.id){
             dispatch(getProductDetails(id));
-            }
+        }
     }, [dispatch, id, product, loading ]);
 
     return (
@@ -40,19 +39,8 @@ const DetailView = () => {
                         <Grid item lg={4} md={4} sm={8} xs={12}>
                             <ActionItem product={product} key={product.id} />
                         </Grid>
-                        <RightComponent item lg={8} md={8} sm={8} xs={12}>
-                            <Typography>{product.title.longTitle}</Typography>
-                            <Typography style={{marginTop:5, color: '#878787', fontSize: 14}}>
-                                8 Ratings & 3 Reviews
-                                <Box component='span'>
-                                    <img src={fassured} alt="fassured" style={{width: 77, marginLeft: 20}} />
-                                </Box>
-                            </Typography>
-                            <Typography>
-                                <Box component='span' style={{fontSize: 28}}>${product.price.cost}</Box>&nbsp;&nbsp;&nbsp;
-                                <Box component='span' style={{color: '#878787'}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
-                                <Box component='span' style={{color: '#388E3C'}}>{product.price.discount}</Box>
-                            </Typography>
+                        <RightComponent item lg={8} md={8} sm={8} xs={12} style={{ paddingLeft: "10px" }}>                            
+                            <ProductDetail product={product}/>
                         </RightComponent>
                     </Container>
             }
