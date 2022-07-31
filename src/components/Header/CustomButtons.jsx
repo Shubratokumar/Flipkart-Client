924912e55c14528ee602e418d89baa16e82b362a
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-07-31 19:06:59 +0600

    custombutton responsive done

--- src/components/Header/CustomButtons.jsx
+++ src/components/Header/CustomButtons.jsx
@@ -5,22 +5,28 @@ import LoginDialog from "./../Login/LoginDialog";
 import { DataContext } from './../../context/DataProvider';
 import Profile from './Profile';
 
-const Wrapper = styled(Box)`
-  display: flex;
-  margin: 0 3% 0 auto;
-  & > button,
-  & > p,
-  & > div {
-    margin-right: 40px;
-    font-size: 15px;
-    font-weight: 600;
-    align-items: center;
+const Wrapper = styled(Box)(({theme})=>({
+  display: 'flex',
+  margin: '0 3% 0 auto',
+  alignItems: 'center',
+  '& > *' : {
+    marginRight: 40,
+    padding: 7,
+    fontSize: 15,
+    fontWeight: 600,
+    alignItems: 'center'
+  },
+  [theme.breakpoints.down('md')] : {
+    display: 'block'
   }
-`;
+}))
 
-const Container = styled(Box)`
-  display: flex;
-`;
+const Container = styled(Box)(({theme})=>({
+  display: 'flex',
+  [theme.breakpoints.down('md')] : {
+    display: 'block'
+  }
+}))
 
 const LoginButton = styled(Button)`
   color: #2874f0;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { AppBar, Toolbar, styled, Box, Typography } from '@mui/material';
+import React, {useState} from 'react';
+import { AppBar, Toolbar, Box, Typography, IconButton, Drawer, List, ListItem, styled  } from '@mui/material';
 import { Link } from 'react-router-dom';
+import {MenuOpen} from '@mui/icons-material';
 
 // components
 import Search from './Search';
@@ -29,18 +30,55 @@ const PlusImage = styled('img')({
     marginLeft: 4,
 })
 
-const CustomButtonWrapper = styled(Box)`
-    margin: 0 5% 0 auto;
-    padding-left: 10px;
-`
+const CustomButtonWrapper = styled(Box)(({theme})=>({
+    margin: '0 5% 0 auto',
+    paddingLeft: 10,
+    [theme.breakpoints.down('md')] : {
+        display: 'none'
+    }
+}));
+
+const MenuButton = styled(IconButton)(({theme}) =>({
+    display:'none',
+    [theme.breakpoints.down('md')] : {
+        display : 'block'
+    }
+}));
+
+
 
 const Header = () => {
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
-    
+
+    const [ open, setOpen ] = useState(false);
+
+    const handleOpen = () =>{
+        setOpen(true);
+    }    
+    const handleClose = () =>{
+        setOpen(false);
+    };
+
+    const MenuList = () => (
+        <Box style={{ width: 200 }} onClick={handleClose}>
+            <List>
+                <ListItem>
+                    <CustomButtons />
+                </ListItem>
+            </List>
+        </Box>
+    )
+
     return (
         <StyledHeader>
             <Toolbar style={{ minHeight: 55 }}>
+                <MenuButton color="inherit"  onClick={handleOpen}>
+                    <MenuOpen />
+                </MenuButton>
+                <Drawer open={open} onClose={handleClose}>
+                    { MenuList() }
+                </Drawer>
                 <Component to='/'>
                     <img src={logoURL} alt="logo" style={{ width: 75 }} />
                     <Box style={{ display: 'flex' }}>
