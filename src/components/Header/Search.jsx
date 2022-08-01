7d4f0833e29e3f6fafe72f244e26992e3ffd36ff
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-08-01 18:10:46 +0600

    Searching functionality done

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { InputBase, Box, styled } from "@mui/material";
+import React, { useEffect, useState} from "react";
+import { InputBase, Box, ListItem, List, styled } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts } from './../../redux/actions/productActions';
+import { Link } from 'react-router-dom';
 
 const SearchContainer = styled(Box)`
   background: #fff;
@@ -20,14 +23,55 @@ const SearchIconWrapper = styled(Box)`
   padding: 5px;
   display: flex;
 `;
+const ListWrapper = styled(List)`
+  position: absolute;
+  background: #ffffff;
+  color: #000;
+  margin-top: 36px;
+`;
 
 const Search = () => {
+  const [ text, setText ] = useState('');
+
+  const { products } = useSelector(state => state.getProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {    
+    dispatch(getProducts())
+  }, [dispatch])
+  
+
+  const getText = (text) => {
+    setText(text);
+  };
   return (
     <SearchContainer>
-      <InputSerchBase placeholder="Search for products, brands and more" />
+      <InputSerchBase 
+        placeholder="Search for products, brands and more"
+        onChange={(e)=> getText(e.target.value)}
+        value={text}
+      />
       <SearchIconWrapper>
         <SearchIcon />
       </SearchIconWrapper>
+      {
+        text && 
+          <ListWrapper>
+              {
+                products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => (
+                  <ListItem>
+                    <Link
+                      to={`/product/${product.id}`}
+                      onClick={()=> setText('')}
+                      style={{ textDecoration: "none", color: 'inherit'}}
+                    >
+                      {product.title.longTitle}
+                    </Link>
+                  </ListItem>
+                ))
+              }
+          </ListWrapper>
+      }
     </SearchContainer>
   );
 };
